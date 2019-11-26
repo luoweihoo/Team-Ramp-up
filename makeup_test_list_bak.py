59ff038d566518cc8dac4f36b6dd25346f776be2
I010809 <wei.luo@sap.com>
2019-11-26 09:59:03 +0800

    Split mandantory & optional participant list

--- makeup_test_list_bak.py
+++ makeup_test_list_bak.py
@@ -18,38 +18,17 @@ workshopNum, workshopCol = trfunction.chooseWorkshop(workshopList)
 
 # Generate the make-up test list and save it to a seperate .xlsx file
 makeupList = str(workshopNum) + '_' + 'MakeupList.xlsx'
-makeupOptionalList = str(workshopNum) + '_' + 'MakeupOptionalList.xlsx'
-
 listWb = openpyxl.Workbook()
 listSheet = listWb.active
-optListWb = openpyxl.Workbook()
-optListSheet = optListWb.active
-
 listRow = 1
-optListRow = 1
-
 sheet = wb.active
 for row in range(4, sheet.max_row + 1):
     if sheet[workshopCol + str(row)].value == None:
-        continue
-    elif sheet[workshopCol + str(row)].value == 'X' :
-        # Generate the mandantory paricipant list
+        pass
+    elif sheet[workshopCol + str(row)].value == 'X' or sheet[workshopCol + str(row)].value == 'X*' or int(sheet[workshopCol + str(row)].value) < 60:
         listSheet['A' + str(listRow)].value = sheet['A' + str(row)].value
         listSheet['B' + str(listRow)].value = sheet['B' + str(row)].value
         listRow = listRow + 1
-    elif sheet[workshopCol + str(row)].value == 'X*':
-        # Generate the optional participant list
-        optListSheet['A' + str(optListRow)].value = sheet['A' + str(row)].value
-        optListSheet['B' + str(optListRow)].value = sheet['B' + str(row)].value
-        optListRow = optListRow + 1
-    elif int(sheet[workshopCol + str(row)].value) < 60:
-        # Generate the mandantory paricipant list
-        listSheet['A' + str(listRow)].value = sheet['A' + str(row)].value
-        listSheet['B' + str(listRow)].value = sheet['B' + str(row)].value
-        listRow = listRow + 1
-    else:
-        continue       
 
 listWb.save(makeupList)
-optListWb.save(makeupOptionalList)
 print('Processing is finished!')
--- workshop_participant_bak.py
+++ workshop_participant_bak.py
@@ -16,35 +16,19 @@ workshopList = trfunction.buildWorkshopList(wb)
 # Ask user to enter the No. of the workshop
 workshopNum, workshopCol = trfunction.chooseWorkshop(workshopList)
 
-# Generate the participant list & optional list and save them into .xlsx files
+# Generate the make-up test list and save it to a seperate .xlsx file
 participantList = str(workshopNum) + '_' + 'List.xlsx'
-participantOptionalList = str(workshopNum) + '_' + 'OptionalList.xlsx'
-
 listWb = openpyxl.Workbook()
 listSheet = listWb.active
-optListWb = openpyxl.Workbook()
-optListSheet = optListWb.active
-
 listRow = 1
-optListRow = 1
-
 sheet = wb.active
 for row in range(4, sheet.max_row + 1):
     if sheet[workshopCol + str(row)].value == None:
-        continue
-    elif sheet[workshopCol + str(row)].value == 'X':
-        # Generate the mandantory paricipant list
+        pass
+    elif sheet[workshopCol + str(row)].value == 'X' or sheet[workshopCol + str(row)].value == 'X*':
         listSheet['A' + str(listRow)].value = sheet['A' + str(row)].value
         listSheet['B' + str(listRow)].value = sheet['B' + str(row)].value
         listRow = listRow + 1
-    elif sheet[workshopCol + str(row)].value == 'X*':
-        # Generate the optional participant list
-        optListSheet['A' + str(optListRow)].value = sheet['A' + str(row)].value
-        optListSheet['B' + str(optListRow)].value = sheet['B' + str(row)].value
-        optListRow = optListRow + 1
-    else:
-        continue
 
 listWb.save(participantList)
-optListWb.save(participantOptionalList)
 print('Processing is finished!')
